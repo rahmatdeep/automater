49ed033065b3fc993b31c7dff73c6a89614a1c69
rahmatdeep <rahmatdeep@gmail.com>
2025-05-04 18:51:33 +0530

    add worker service

---
+++ apps/worker/src/index.ts
@@ -0,0 +1,29 @@
+import { kafka, TOPIC_NAME } from "@repo/kafka/kafka-client";
+
+(async () => {
+  while (true) {
+    const consumer = kafka.consumer({ groupId: "main-worker" });
+    await consumer.connect();
+
+    await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true });
+
+    await consumer.run({
+      autoCommit: false,
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value?.toString(),
+        });
+        await new Promise((r) => setTimeout(r, 1000));
+        await consumer.commitOffsets([
+          {
+            topic: TOPIC_NAME,
+            partition,
+            offset: (parseInt(message.offset) + 1).toString(),
+          },
+        ]);
+      },
+    });
+  }
+})();
